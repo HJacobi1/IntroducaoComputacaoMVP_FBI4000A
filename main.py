9a1c22fc903c84d127bb2f75790840822f17d669
HJacobi1 <henrique.jacobi1@gmail.com>
2024-07-01 02:06:16 -0300

    Finalização dos UC funcionário/freela e andamento no UC gestor

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from math import e
 from time import sleep
 from pathlib import Path
 
-# Dicionário para armazenar os registros de ponto
-registros_ponto = {}
-
 indFuncao = 0
 nome = ""
 empresa = ""
@@ -43,17 +40,134 @@ def ler_pontos(funcionario_id):
     except(FileNotFoundError):
         print("Não foi encontrado registro de ponto para o id",str(funcionario_id))
 
-def calcular_diferenca_horas(funcionario_id):
+def consultar_ponto_completo(funcionario_id):
     registros = ler_pontos(funcionario_id)
-    
-    if len(registros) < 2:
-        print("Não há registros suficientes para calcular a diferença de horas.")
-        return
 
-    for i in range(1, len(registros)):
-        diferenca = registros[i] - registros[i - 1]
-        diferenca_em_horas = diferenca.total_seconds() / 3600
-        print(f"Diferença entre {registros[i - 1]} e {registros[i]}: {diferenca_em_horas:.2f} horas")
+    num_registros = len(registros)
+    
+    if num_registros < 2:
+        print("Não há registros suficientes para calcular a diferença de horas.")
+        return    
+
+    dia = datetime.min
+    minutos_dia = 0
+    total_trabalhado = 0
+    total_diferenca = 0
+
+    for i in range(1, num_registros,2):
+        if i > num_registros:
+            return
+        
+        if dia.day != registros[i].day:            
+            if minutos_dia > 0:
+                diferenca_dia = calcula_diferenca(minutos_dia)
+                total_diferenca += diferenca_dia
+                imprime_saldo_dia(minutos_dia, diferenca_dia)
+                minutos_dia = 0                
+
+            dia = registros[i]
+            print(f"Dia {registros[i].day:02}/{registros[i].month:02}/{registros[i].year}:")
+            print("Horários registrados:")
+
+        diferenca = registros[i] - registros[i - 1]        
+
+        minutos_dia += diferenca.total_seconds() // 60
+
+        diferenca_em_horas = diferenca.total_seconds() // 3600
+        diferenca_em_minutos = (diferenca.total_seconds() % 3600) // 60
+        print(f"Tempo trabalhado: {int(diferenca_em_horas):02}:{int(diferenca_em_minutos):02}")
+
+        total_trabalhado += diferenca.total_seconds() // 60
+
+    if minutos_dia > 0:
+        diferenca_dia = calcula_diferenca(minutos_dia)
+        total_diferenca += diferenca_dia
+        imprime_saldo_dia(minutos_dia,diferenca_dia)
+    
+    print("--------- Totais ---------")
+    print(f"Horas trabalhadas: {int((total_trabalhado//60)):02}:{int((total_trabalhado%60)):02}")
+    if total_diferenca > 0:
+        devido_extra = "devidas"
+    else:
+        total_diferenca *= -1
+        devido_extra = "extras"
+
+    print(f"Diferença final: {total_diferenca//60:02}:{total_diferenca%60:02} {devido_extra}")
+    print("--------------------------")    
+    print()
+
+def calcula_diferenca(minutos_dia):
+    global carga_horaria
+
+    if carga_horaria > 0:
+        return int(carga_horaria*60 - minutos_dia)
+    
+    return minutos_dia
+
+def imprime_saldo_dia(minutos_dia, diferenca_minutos):
+    global indFuncao
+    global carga_horaria
+    global salario_hora
+
+    horas_trabalhadas = int(minutos_dia // 60)
+    minutos_trabalhados = int(minutos_dia % 60)
+
+    print()
+    print("------ Saldo do dia ------")
+    print(f"Horas trabalhadas: {horas_trabalhadas:02}:{minutos_trabalhados:02}")
+    if indFuncao == 3: #Se for freela, mostra valor a receber
+        print(f"Valor a receber: ${salario_hora*minutos_dia/60:.2f}")
+    else:
+        print(f"Carga horária normal: {carga_horaria:02}h")        
+        if diferenca_minutos == 0:
+            print(f"Carga horária cumprida.")
+        elif diferenca_minutos > 0:
+            print(f"São devidas {(diferenca_minutos//60):02}:{(diferenca_minutos%60):02}!")
+        else:
+            diferenca_minutos *= -1
+            print(f"Foram feitas {(diferenca_minutos//60):02}:{(diferenca_minutos%60):02} extras!")
+    print()
+
+def consultar_ponto_completo_dia(funcionario_id):
+    registros = ler_pontos(funcionario_id)
+    dia_hist = 0
+
+    print("Dias para consulta:")
+
+    for i in range(0,len(registros)):        
+        dia = registros[i]
+
+        if dia.day != dia_hist:
+            dia_hist = dia.day
+            print(f"{i}: {dia.day:02}/{dia.month:02}/{dia.year}")
+    
+    index_dia = int(input("Informe o dia para consulta: "))
+
+    consultar_dia(registros,index_dia)
+
+def consultar_dia(registros, index):
+    instancia = registros[index]
+    minutos_dia = 0
+
+    print()
+    print(f"Dia {instancia.day:02}/{instancia.month:02}/{instancia.year}")
+    print("Horários registrados:")
+    for i in range(1,len(registros),2):
+        dia = registros[i].day
+        if dia != instancia.day:        
+            continue
+        else:
+            diferenca = registros[i] - registros[i - 1]        
+
+            minutos_dia += diferenca.total_seconds() // 60
+
+            diferenca_em_horas = diferenca.total_seconds() // 3600
+            diferenca_em_minutos = (diferenca.total_seconds() % 3600) // 60
+            print(f"Tempo trabalhado: {int(diferenca_em_horas):02}:{int(diferenca_em_minutos):02}")
+
+    if minutos_dia > 0:
+        diferenca_dia = calcula_diferenca(minutos_dia)
+        imprime_saldo_dia(minutos_dia,diferenca_dia)
 
 def ler_todos_arquivos_ponto():
     # Caminho para a pasta raiz do programa
@@ -64,31 +178,52 @@ def ler_todos_arquivos_ponto():
 
     for file in files:
         if file.is_file():
-            with open(file, 'r') as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    print(row)
+            id = extrair_id(file.name)
+            print(f"Colaborador Matrícula {id}:")
+            consultar_ponto_completo(id)
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~")            
+
+def extrair_id(nome_arquivo):
+    aux = nome_arquivo.split('_')
+    return aux[1].split('.')[0]
 
 # Função para alterar o ponto de um funcionário (apenas para gestores)
-def alterar_ponto_gestor(funcionario_id):
-    if funcionario_id in registros_ponto:
-        print(f'Registro atual para funcionário {funcionario_id}:')
-        print(f'Entrada: {registros_ponto[funcionario_id]["entrada"]}')
-        print(f'Saída: {registros_ponto[funcionario_id]["saida"]}')
-        opcao = input('Deseja alterar o registro? (s/n): ')
-        if opcao.lower() == 's':
-            nova_entrada = input('Digite a nova hora de entrada (formato HH:MM): ')
-            nova_saida = input('Digite a nova hora de saída (formato HH:MM): ')
-            try:
-                registros_ponto[funcionario_id]['entrada'] = datetime.datetime.strptime(nova_entrada, '%H:%M')
-                registros_ponto[funcionario_id]['saida'] = datetime.datetime.strptime(nova_saida, '%H:%M')
-                print(f'Registro alterado com sucesso para funcionário {funcionario_id}')
-            except ValueError:
-                print('Formato de hora inválido. Use HH:MM.')
-        else:
-            print('Registro não alterado.')
+def ajustar_ponto_funcionario(id):
+    registros = ler_pontos(id)
+    print("Escolha o registro que deseja alterar:")
+    for i in range(0,len(registros)):
+        print(f"{i}: {registros[i].day:02}/{registros[i].month:02}/{registros[i].year} {registros[i].hour:02}:{registros[i].minute:02}")
+    print(f"{i+1}: Adicionar novo registro")
+    index = int(input())
+
+    nova_data = input("Infome a nova data (dd/mm/yyyy): ")
+    nova_hora = input("Infome a nova hora (hh:mm): ")
+
+    confirma = input("Confirma alteração? (s/n) ")
+
+    if confirma.lower() == 's':
+        data_split = nova_data.split('/')
+        hora_split = nova_hora.split(':')
+        nova_data = datetime(int(data_split[2]),int(data_split[1]),int(data_split[0]),int(hora_split[0]),int(hora_split[1]))
+        modificar_ponto(id, index, nova_data)
+
+# Função para modificar uma linha específica
+def modificar_ponto(funcionario_id, index, nova_data):
+    nomeArquivo = f"ponto_{str(funcionario_id)}.csv"
+    registros = ler_pontos(funcionario_id)
+    if index >= len(registros):
+        registros.append(nova_data)
     else:
-        print(f'Funcionário {funcionario_id} não possui registro de ponto.')
+        registros[index] = nova_data
+
+    with open(nomeArquivo, mode="w", newline='', encoding='utf-8') as arquivo:
+        writer = csv.writer(arquivo, quoting=csv.QUOTE_NONE)
+        for registro in registros:
+            hora = f"{registro.hour:02}:{registro.minute:02}"
+            dia = f"{registro.day:02}/{registro.month:02}/{registro.year}"
+            writer.writerow([dia,hora])
+
+    print(f"Linha {index} modificada com sucesso.")
 
 def montaArquivoConfig():
     arqConfig = open("config.dat","w")
@@ -162,7 +297,7 @@ def realiza_acao_um():
         hora_str = f"{agora.day:02}/{agora.month:02}/{agora.year:4} - {agora.hour:02}:{agora.minute:02}"
 
         print("Confirma marcação de ponto ",hora_str,"?(s/n)")
-        acao = input()
+        acao = input().lower()
         if acao == "s":
             registrar_ponto(id)
             print("Ponto Registrado com sucesso!")
@@ -172,21 +307,23 @@ def realiza_acao_dois():
     global id
 
     if(indFuncao == 1):
-        return
+        id = int(input("Informe a matrícula do colaborador que deseja alterar: "))
+        ajustar_ponto_funcionario(id)
     else:
-        registros = ler_pontos(id)
-        for registro in registros:
-            print(registro)
-
-        calcular_diferenca_horas(id)
+        consultar_ponto_completo(id)
 
 
 def realiza_acao_tres():
-    foo = 0
+    global indFuncao
+    global id
 
-
+    if(indFuncao == 1):
+        return
+    else:
+        consultar_ponto_completo_dia(id)
 
 # Menu principal
+print()
 print("==========================")
 print(" ----- HoraCerta.py ----- ")
 print("==========================")
@@ -199,23 +336,27 @@ try:
 except(FileExistsError):
     lerArquivoConfig()
 
-print(f"Seja bem vindo(a){nome}!")
+print(f"Seja bem vindo(a) {nome}")
 
 while True:
     opcao = -1
     print('Menu de opções - Selecione a ação que deseja realizar:')
     match indFuncao:
         case 1: # gestor
-            print('1 - Consultar ponto do funcionário')
+            print('1 - Consultar todos os pontos')
+            print('2 - Ajustar ponto')
         case 2: # funcionario
             print('1 - Marcar ponto')
-            print('2 - Conferir pontos')
+            print('2 - Consultar pontos')
+            print('3 - Consultar dia específico')
         case 3: # freelancer
             print('1 - Marcar ponto')
-            print('2 - Conferir pontos')
+            print('2 - Consultar pontos')
+            print('3 - Consultar dia específico')
 
     print('0 - Encerrar programa')
     opcao = int(input('Digite a sua opção: '))
+    print()
 
     if opcao == 1:
         realiza_acao_um()
@@ -229,5 +370,4 @@ while True:
         break
     else:
         print('Opção inválida. Tente novamente.')
-    print('')
-    
+    print('')    
